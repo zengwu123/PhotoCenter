33568344b35ab7d5ad51143b2bb3cc6ea9f67022
zengwu123 <1203287595@qq.com>
2020-06-06 18:52:56 +0800

    hello word2

--- src/main/java/com/photo/center/PhotoCenterApplication.java
+++ src/main/java/com/photo/center/PhotoCenterApplication.java
@@ -8,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PhotoCenterApplication {
 
 	public static void main(String[] args) {
+		System.out.println("fsdfsadfsaf");
 		SpringApplication.run(PhotoCenterApplication.class, args);
 	}
 
